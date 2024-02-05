86480d853d2f4fd7b281ea22a5204f48f9bed5bc
kyo21 <kussa.laksana@gmail.com>
2024-02-05 13:25:52 +0800

    adding file cpp and pdf

---
+++ berkebun_jagung.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+
+using namespace std;
+//<--------dilarang menambahkan kode di atas batas ini ----->>
+
+int bisa_ditanam_jagung(){
+
+
+}
+
+
+int main(){
+
+return 0;
+}
